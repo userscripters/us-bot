2dfd854595ed84a84ce28010686b313cfa412bbd
Oleg Valter <o.a.philippov@gmail.com>
2021-09-10 04:55:14 +0300

    added MD link helper

--- src/helpers.ts
+++ src/helpers.ts
@@ -2,3 +2,8 @@
  * @summary splits ENV field into an array of strings
  */
 export const splitENV = (env = "") => env.split(/\s*\|\s*/);
+
+/**
+ * @summary makes a markdown link
+ */
+export const mdLink = (url: string, title = url) => `[${title}](${url})`;
