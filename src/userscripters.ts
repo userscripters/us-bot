360f73e99cffc584fe43f798186fb8ad1b5d36c7
Oleg Valter <o.a.philippov@gmail.com>
2021-09-16 01:16:39 +0300

    added PKEY env variable for passing private keys as env vars

--- src/userscripters.ts
+++ src/userscripters.ts
@@ -5,15 +5,16 @@ import { Octokit } from "octokit";
 
 dotenv.config();
 
-const { APP_ID, CLID, SECRET, INSTALL_ID } = process.env;
+const { APP_ID, CLID, SECRET, INSTALL_ID, PKEY } = process.env;
 
-const PKEY = await readFile("./pkey.pem", { encoding: "utf-8" });
+const privateKey =
+    PKEY || (await readFile("./pkey.pem", { encoding: "utf-8" }));
 
 const oktokit = new Octokit({
     authStrategy: createAppAuth,
     auth: {
         appId: APP_ID!,
-        privateKey: PKEY,
+        privateKey,
         clientId: CLID,
         clientSecret: SECRET,
         installationId: INSTALL_ID!,
