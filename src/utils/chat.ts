3f2805cffd7e0b664555afd4a568df4f0afd5e3a
Oleg Valter <47309194+O-Valter@users.noreply.github.com>
2021-12-11 01:41:29 +0300

    added helper for stripping out leading @-mention

---
+++ src/utils/chat.ts
@@ -0,0 +1,9 @@
+/**
+ * @summary strips chat message leading @-mention
+ * @param text text to process
+ * @param name @-mention to strip out
+ */
+export const stripLeadingMention = (text: string, name: string) => {
+    const nameexpr = new RegExp(`^@${name}:?\\s+`, "i");
+    return text.replace(nameexpr, "");
+}
