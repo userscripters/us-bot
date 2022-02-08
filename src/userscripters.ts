71cf78493fa3280ace2624001d7fd6aa1c4e4b88
Oleg Valter <o.a.philippov@gmail.com>
2022-02-08 03:44:31 +0300

    added helpers for get a map of columns for projects/organizations

--- src/userscripters.ts
+++ src/userscripters.ts
@@ -2,6 +2,7 @@ import { createAppAuth } from "@octokit/auth-app";
 import dotenv from "dotenv";
 import { readFile } from "fs/promises";
 import { Octokit } from "octokit";
+import ArrayMap from "./utils/arraymap.js";
 
 dotenv.config();
 
@@ -20,5 +21,39 @@ const oktokit = new Octokit({
         installationId: INSTALL_ID!,
     },
 });
+
+/**
+ * @summary gets a {@link ArrayMap} of all org project columns
+ * @param org name of the organization to get columns for
+ */
+export const getOrgColumns = async (org: string) => {
+    const { data: projects } = await oktokit.rest.projects.listForOrg({ org });
+
+    const columnMap: ArrayMap<string, number> = new ArrayMap();
+
+    for (const { id } of projects) {
+        const columns = await getProjectColumns(id);
+        columnMap.join(columns);
+    }
+
+    return columnMap;
+};
+
+/**
+ * @summary gets a {@link ArrayMap} of project columns
+ * @param projectId project_id of the project
+ */
+export const getProjectColumns = async (projectId: string | number) => {
+    const colRes = await oktokit.rest.projects.listColumns({
+        project_id: +projectId
+    });
+
+    const columnMap: ArrayMap<string, number> = new ArrayMap();
+
+    const columns = colRes.data;
+    columns.forEach(({ id, name }) => columnMap.set(name, id));
+
+    return columnMap;
+};
 
 export default oktokit;
